0a7253c5717eccc6656e139894595365ba385d52
Younes <younesaitsiabou@gmail.com>
2025-12-22 09:34:55 +0100

    function multiply added

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function sum(a, b) {
   return a + b;
 }
+
 function substract(a, b) {
   return a - b;
 }
 
-module.exports = {sum, substract};
+function multiply(a, b){
+  return a*b
+}
+
+module.exports = {sum, substract, multiply};
--- script.test.js
+++ script.test.js
@@ -1,9 +1,13 @@
-const {sum, substract} = require('./script.js');
+const {sum, substract, multiply} = require('./script.js');
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
 
-test('substract 1 from 2 to equal -1', () => {
-  expect(substract(1, 2)).toBe(-1);
+test('substract 1 from 2 to equal 1', () => {
+  expect(substract(2, 1)).toBe(1);
+});
+
+test('multiply 2 by 2 to be 4', () => {
+  expect(multiply(2, 2)).toBe(4);
 });
